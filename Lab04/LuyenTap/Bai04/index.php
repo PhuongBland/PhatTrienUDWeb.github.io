5c04645b343b8d53b264f0f7852c304b2b760fc0
Phương Võ ♋ <63140726+PhuongBland@users.noreply.github.com>
2021-10-12 23:30:30 +0700

    Bai04-lt

---
+++ Lab04/LuyenTap/Bai04/index.php
@@ -0,0 +1,31 @@
+<?php
+class BankAccount{
+    private $balance;
+    public function getBalance(){
+        return $this->balance;
+    }
+    public function deposit($amount){
+        if($amount>0){
+            $this->balance+=$amount;
+        }
+        return $this;
+    }
+}
+class SavingAccount extends BankAccount{
+    private $interestRate;
+    public function setInterestRate($interestRate){
+        $this ->interestRate=$interestRate;
+    }
+
+    public function addInterest(){
+        $interest=$this->interestRate*$this->getBalance();
+        $this->deposit($interest);
+    }
+}
+$account = new SavingAccount();
+$account->deposit(100);
+// set interest rate
+$account->setInterestRate(0.05);
+$account->addInterest();
+echo $account->getBalance();  
+ ?>
